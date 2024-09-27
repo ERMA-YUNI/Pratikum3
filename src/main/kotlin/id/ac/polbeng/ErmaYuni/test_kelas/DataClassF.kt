013e485f9b99582bbdcc3c55776768d864f31cbe
ERMA-YUNI <eyuni0464@gmail.com>
2024-09-27 12:19:20 +0700

    add       DataClassF.kt

---
+++ src/main/kotlin/id/ac/polbeng/ErmaYuni/test_kelas/DataClassF.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.ErmaYuni.test_kelas
+
+fun main(){
+    val meri = Student("meri", 20)
+    val name = meri.component1()
+    val age = meri.component2()
+    println("Name = $name")
+    println("Age = $age")
+}
